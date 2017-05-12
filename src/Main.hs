1a39ff4cae59e12ca4397c9f2962c5a6ce496a88
Zach Sullivan <zachsully@gmail.com>
2017-05-12 13:20:01 -0700

    add way to test that outputs are produced

--- src/Main.hs
+++ src/Main.hs
@@ -1,3 +1,6 @@
+import Data.Monoid
+import Data.Maybe (isJust, fromJust)
+-- import Options.Applicative
 import System.IO
 import System.Process
 import System.Exit
@@ -5,30 +8,32 @@ import System.Directory
 import System.FilePath.Posix
 import System.Environment
 
-import Data.Monoid
-import Data.Maybe (isJust, fromJust)
-
 main :: IO ()
 main = do
   args <- getArgs
   let (hkc,cc) = case args of
                   (hkc:cc:[]) -> (hkc,cc)
                   _           -> error "Usage: hkc-tests <HKC> <CC>"
+
   tests   <- getTests
-  createBuildDirectory
+  createDirectoryIfMissing False "build"
+
   putStrLn $ stars <> "TESTING HKC" <> stars
   tests' <- mapM (hakaruToC hkc) tests
+
   putStrLn $ stars <> "TESTING CC" <> stars
   tests'' <- mapM (cToBinary cc . fromJust) . filter isJust $ tests'
+
+  -- putStrLn $ stars <> "TESTING OUTPUT" <> stars
+  -- tests''' <- mapM (binaryToOutput . fromJust) . filter isJust $ tests''
+
   reportStats "HKC Tests" tests'
   reportStats "CC Tests" tests''
+  -- reportStats "Output Tests" tests'''
   putStrLn "Fin."
 
 getTests :: IO [FilePath]
 getTests = filter ((== ".hk") . takeExtension) <$> listDirectory "tests/hakaru"
-
-createBuildDirectory :: IO ()
-createBuildDirectory = createDirectoryIfMissing False "build"
 
 getStats :: [Maybe a] -> (Int,Int)
 getStats xs =
@@ -43,6 +48,13 @@ reportStats test xs =
            <> (show success) <> " of " <> (show total)
            <> "\n" <> stars
    where (success,total) = getStats xs
+
+stars :: String
+stars = "\n" <> replicate 80 '*' <> "\n"
+
+--------------------------------------------------------------------------------
+--                                 TESTS                                      --
+--------------------------------------------------------------------------------
 
 hakaruToC :: String -> FilePath -> IO (Maybe FilePath)
 hakaruToC hkc fp =
@@ -62,6 +74,7 @@ hakaruToC hkc fp =
                  appendFile "build/hkc.log" (stars <> fp <>":\n" <> err)
                  return Nothing
 
+-- Check to see if it produces legal C code
 cToBinary :: String -> FilePath -> IO (Maybe FilePath)
 cToBinary cc fp =
   let fp' = "build" </> "bin" </> (takeBaseName fp)
@@ -79,8 +92,19 @@ cToBinary cc fp =
                  appendFile "build/cc.log" (stars <> fp <>":\n" <> err)
                  return Nothing
 
-stars :: String
-stars = "\n" <> replicate 80 '*' <> "\n"
-
-binaryToOutput :: FilePath -> IO FilePath
-binaryToOutput = undefined
+-- we will want the type of the program for testing output
+binaryToOutput :: FilePath -> IO (Maybe FilePath)
+binaryToOutput fp =
+  let fp' = "build" </> "output" </> fp </> "out"
+      process = proc fp [">",fp']
+  in
+    do createDirectoryIfMissing False ("build" </> "output")
+       putStrLn $ "output: ( " <> fp <> " , " <> fp' <> " )"
+       (_,_,Just errH,pH) <- createProcess process { std_err = CreatePipe }
+       exitcode <- waitForProcess pH
+       case exitcode of
+         ExitSuccess -> return (Just fp')
+         _ -> do putStrLn "FAILED"
+                 err <- hGetContents errH
+                 appendFile "build/output.log" (stars <> fp <>":\n" <> err)
+                 return Nothing
